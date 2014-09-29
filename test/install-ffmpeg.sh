02a15223f06c204bcd0929721b96035c47e1b5ce
cytec <iamcytec@googlemail.com>
2014-09-29 15:37:26 +0200

    Update install-ffmpeg.sh

--- test/install-ffmpeg.sh
+++ test/install-ffmpeg.sh
@@ -4,7 +4,7 @@
 # add-apt-repository -y ppa:jon-severinsson/ffmpeg
 # apt-get -y -qq update
 # apt-get -y -qq install ffmpeg
-if [ $(arch) == "i686" ]:
+if [ $(arch) == "i686" ]; then
   wget -P /tmp http://ffmpeg.gusari.org/static/32bit/ffmpeg.static.32bit.latest.tar.gz
   tar -xvf /tmp/ffmpeg.static.32bit.lates.tar.gz -C /usr/local/bin
 else
